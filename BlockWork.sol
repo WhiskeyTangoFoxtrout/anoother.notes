6e504cec40117bd3fb939224d3803d183887e886
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-03 15:00:29 -0600

    Create BlockWork.sol

---
+++ BlockWork.sol
@@ -0,0 +1,32 @@
+//need somewhere to work on 
+//this is also one way I take notes so bare wit me its gone grow
+pragma solidity >= 0.3.5 < 0.9.0
+//null isnt a thing you have to set it to 0 This a fundemental lingo. Math err thang math
+contract idkYetSometypeofVerifacation_justLikeErrthangElse{
+
+    modifier Lets {
+
+        require(//be a thing);
+    
+    }
+
+}
+
+function blank(){
+
+    //I actually gotta study the namespaces bc its what dictate what i can do. Its like ASM... annoyance
+    emit IsHowYouCallIt(msg.sender,msg.value);
+    struct BushIt{
+
+        uint weight
+        uint height
+        bool male_or_female
+        address DataBase
+
+    }
+
+    enum Condition{Active, Idle, Running} //enum has finite or set amount of values... think python
+    
+}
+
+event IsHowYouCallIt(0);
